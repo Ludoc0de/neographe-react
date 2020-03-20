426ee59f34dcfacc0ac840873761afce9dde9e7a
asakuracl <champi.ludo@hotmail.fr>
2020-03-20 20:27:09 -0300

    move files

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Header extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <h1>test</h1>
+        )
+    }
+}
+
+export default Header
