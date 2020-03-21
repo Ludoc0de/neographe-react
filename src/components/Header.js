42475901af25ee1dfe06fcef8c079362abd382c0
asakuracl <champi.ludo@hotmail.fr>
2020-03-20 22:19:30 -0300

    create navbar add condition on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,33 @@ import React from 'react'
 class Header extends React.Component {
     constructor() {
         super();
+        this.state = {
+            title: 'NEOGRAPHE',
+            moonLight: true
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            moonLight: !prevState.moonLight
+        }))
     }
 
     render() {
         return (
-            <header>test</header>
+            <header>
+                <p className='header-p'>{this.state.title}</p>
+                <div>
+                    <i className="blackMoon fas fa-moon" onClick={this.handleClick}
+                        style={{ display: this.state.moonLight ? 'none' : 'inline-block' }}>
+                    </i>
+                    <i className="clearMoon far fa-moon" onClick={this.handleClick}
+                        style={{ display: this.state.moonLight ? 'inline-block' : 'none' }}>
+
+                    </i>
+                </div>
+            </header>
         )
     }
 }
