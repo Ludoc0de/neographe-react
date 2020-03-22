dcdf18e06155c3e8fdc1d3d1ba49e7f9d2472946
asakuracl <champi.ludo@hotmail.fr>
2020-03-22 00:08:56 -0300

    add Footer add className

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends React.Component {
+  /*
   constructor() {
     super();
   }
-
+*/
   render() {
     return (
-      <Header />
+      <div className='App-div'>
+        <Header />
+        <Footer />
+      </div>
     )
   }
 }
