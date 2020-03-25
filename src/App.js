401514cc4c9cc0da98b3f5fc4e1fa4d528d1772d
asakuracl <champi.ludo@hotmail.fr>
2020-03-25 10:03:34 -0300

    update name in import, render component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import Header from './components/Header';
-import Footer from './components/Footer';
+import HeaderFooter from './components/HeaderFooter';
+
 
 class App extends React.Component {
   /*
   constructor() {
     super();
   }
-*/
+  */
+
   render() {
     return (
       <div className='App-div'>
-        <Header />
-        <Footer />
-      </div>
+        <HeaderFooter />
+      </div >
     )
   }
 }
