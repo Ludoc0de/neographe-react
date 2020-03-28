2148ae0c505cd70c0efbf78f91dab23b4c14620f
asakuracl <champi.ludo@hotmail.fr>
2020-03-28 01:06:12 -0300

    add state, lifecycle, and condition

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,16 +2,50 @@ import React, { Component } from 'react'
 import DarkLogo from '../images/logo-black.png'
 
 class MainContent extends Component {
-    /*
     constructor(){
         super()
+        this.state = {
+            mainLog:'bloc',
+            anime: '',
+            LogoMirror:'none' 
+        }
+        this.handleLoad = this.handleLoad.bind(this)
     }
-    */
+
+    componentDidMount(){
+        window.addEventListener('load', this.handleLoad)
+        console.log('1')
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('load', this.handleLoad)
+        console.log('awA')
+    }
+
+    handleLoad(){
+        setTimeout(() => {
+        this.setState(prevState => {
+            return {
+                anime: 'none',
+                LogoMirror: 'bloc'  
+            }
+        })
+        }, 3000)
+    }
+
     render(){
         return(
             <div className='mainContent'>
-                <img className='mainLogo' src={DarkLogo}/>
-                <img className='mainLogoMirror' src={DarkLogo} />
+                <img className='mainLogo' src={DarkLogo}  
+                    style={{ display: this.state.mainLog, animation:this.state.anime}}
+                />
+                {
+                    this.state.anime && 
+                    <img className='mainLogoMirror' src={DarkLogo}
+                        style={{ display: this.state.LogoMirror }}
+                    />
+                }
+                
             </div>
         )
     }
