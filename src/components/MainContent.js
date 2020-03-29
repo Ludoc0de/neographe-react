7a07b5395a3d905f4ee467dfc896d7ef1baee04c
asakuracl <champi.ludo@hotmail.fr>
2020-03-29 20:47:40 -0300

    add new state for loader, still working on it

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,46 +1,60 @@
 import React, { Component } from 'react'
 import DarkLogo from '../images/logo-black.png'
+import LoaderImage from '../images/loader.svg'
 
 class MainContent extends Component {
     constructor(){
         super()
         this.state = {
-            mainLog:'bloc',
-            anime: '',
+            loaderImage:'',
+            loader: '',
+            mainLog:'none',
+            anime: 'none',
             LogoMirror:'none' 
         }
         this.handleLoad = this.handleLoad.bind(this)
     }
 
+    
     componentDidMount(){
         window.addEventListener('load', this.handleLoad)
-        console.log('1')
     }
 
     componentWillUnmount(){
         window.removeEventListener('load', this.handleLoad)
-        console.log('awA')
     }
+
+
 
     handleLoad(){
         setTimeout(() => {
         this.setState(prevState => {
             return {
-                anime: 'none',
-                LogoMirror: 'bloc'  
+                loaderImage: 'none',
+                loader: 'none',
+                mainLog: '',
+                anime: '',
+                LogoMirror: 'bloc'
             }
         })
-        }, 3000)
+        }, 1000)
     }
 
     render(){
         return(
             <div className='mainContent'>
+                <img className='loaderImage' src={LoaderImage} 
+                    style={{ display: this.state.loaderImage }} />
+                <svg className='loader'>
+                    <circle cx="80" cy="80" r="65" style={{animation: this.state.loader}}></circle>
+                </svg>
+
                 <img className='mainLogo' src={DarkLogo}  
                     style={{ display: this.state.mainLog, animation:this.state.anime}}
                 />
                 {
-                    this.state.anime && 
+                    this.state.anime ? 
+                    console.log('tezst') :
                     <img className='mainLogoMirror' src={DarkLogo}
                         style={{ display: this.state.LogoMirror }}
                     />
