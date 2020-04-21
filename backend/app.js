7252d91400cb9ff6e17df4837c04c4fc4730ec23
asakuracl <champi.ludo@hotmail.fr>
2020-04-21 14:21:11 -0300

    add post on app router

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,16 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.post('/api/form', (req, res, next) => {
+  
+    const formData = req.body
+    connection.query("INSERT INTO form SET ?", formData, (error, results, fields) =>{
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    });
+});
+
+
 app.get('/get-form', (req, res, next)=> {
     connection.query('SELECT * FROM form', (error, results, fields) => {
         if (error) throw error;
