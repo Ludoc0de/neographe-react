c50cfd00dddf965a241380fb0570b5d8e40139df
asakuracl <champi.ludo@hotmail.fr>
2020-04-26 20:29:03 -0300

    Remove connection to mysql add in manager. Remove all post/get add in router.

--- backend/app.dist.js
+++ backend/app.dist.js
@@ -1,21 +1,9 @@
-//hold version
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: ''
-})
-connection.connect((err) => {
-    (err) ? console.log('error connection') : console.log('success connection')
-})
+
 
 const app = express()
-
-//let forms = require("./db")
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,33 +13,6 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-
-// for update
-//app.post('/api/form', (req, res, next) => {
-//     const formData = req.body
-//     connection.query("INSERT INTO form SET ?", formData, (error, results, fields) => {
-//         if (error) throw error;
-//         res.send(JSON.stringify(results));
-//     });
-// });
-
-app.post('/api/form', (req, res, next) => {
-
-    const sql = "INSERT INTO form (firstname, lastname, mail, subject, textarea) VALUES ('" + req.body.firstName + "','" + req.body.lastName + "','" + req.body.mail + "','" + req.body.subject + "','" + req.body.textArea + "')"
-
-    connection.query(sql, (error, results, fields) => {
-        if (error) throw error;
-        res.send(JSON.stringify(results));
-    });
-});
-
-
-app.get('/api/form', (req, res, next) => {
-    connection.query('SELECT * FROM form', (error, results, fields) => {
-        if (error) throw error;
-        res.send(JSON.stringify(results));
-    });
-});
 
 
 module.exports = app
