e2ccfa16d22e0a087e1ab6ae558b5fc22d9bce8d
asakuracl <champi.ludo@hotmail.fr>
2020-04-26 20:44:58 -0300

    remove all get/post to be only a router.

--- backend/routes/form.js
+++ backend/routes/form.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const connection = require('../manager')
+
+const formControllers = require('../controllers/form')
+
+router.post('/', formControllers.createForm);
 
 
-router.post('/', (req, res, next) => {
-
-    const sql = "INSERT INTO form (firstname, lastname, mail, subject, textarea) VALUES ('" + req.body.firstName + "','" + req.body.lastName + "','" + req.body.mail + "','" + req.body.subject + "','" + req.body.textArea + "')"
-
-    connection.query(sql, (error, results, fields) => {
-        if (error) throw error;
-        res.send(JSON.stringify(results));
-    });
-});
-
-
-router.get('/', (req, res, next) => {
-    connection.query('SELECT * FROM form', (error, results, fields) => {
-        if (error) throw error;
-        res.send(JSON.stringify(results));
-    });
-});
+router.get('/', formControllers.getAllForm);
 
 module.exports = router
