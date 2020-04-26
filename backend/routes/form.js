07fa7a93b22c406b22314238d25098b491e8dff9
asakuracl <champi.ludo@hotmail.fr>
2020-04-26 01:29:02 -0300

    get all app/post/get & change app to router. Remove path way in router.

--- backend/routes/form.js
+++ backend/routes/form.js
@@ -1,2 +1,23 @@
 const express = require('express')
 const router = express.Router()
+
+
+router.post('/', (req, res, next) => {
+
+    const sql = "INSERT INTO form (firstname, lastname, mail, subject, textarea) VALUES ('" + req.body.firstName + "','" + req.body.lastName + "','" + req.body.mail + "','" + req.body.subject + "','" + req.body.textArea + "')"
+
+    connection.query(sql, (error, results, fields) => {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    });
+});
+
+
+router.get('/', (req, res, next) => {
+    connection.query('SELECT * FROM form', (error, results, fields) => {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    });
+});
+
+module.exports = router
