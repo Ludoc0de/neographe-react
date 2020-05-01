d8e0d9ad1bc1cc81e7c917ce2ad50a02d4661f6a
asakuracl <champi.ludo@hotmail.fr>
2020-05-01 20:05:56 -0300

    create login.js

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class Login extends React.Component {
+    /*
+    constructor() {
+      super();
+    }
+    */
+
+    render() {
+        return (
+            <h2>Test</h2>
+        )
+    }
+}
+
+export default Login
