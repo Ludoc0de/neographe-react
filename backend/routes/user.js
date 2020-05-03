91ffab5b921e63feba5667a3789519ec7b0037f5
asakuracl <champi.ludo@hotmail.fr>
2020-05-03 11:41:04 -0300

    fix controllerUser apostrophe

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const controllersUser = require('../controllers/user)
+const controllersUser = require('../controllers/user')
 
 router.post('/', controllersUser.signup);
 router.post('/', controllersUser.login);
