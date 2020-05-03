fa9f63e93d35eb1ea8ae15442a82b48c7cae9d9b
asakuracl <champi.ludo@hotmail.fr>
2020-05-03 11:12:11 -0300

    get the controller & add route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const controllersUser = require('../controllers/user)
+
+router.post('/', controllersUser.signup);
+router.post('/', controllersUser.login);
 
 module.exports = router
