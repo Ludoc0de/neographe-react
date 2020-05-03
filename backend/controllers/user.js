35abff1d1c7a6bc52ec2c3a1eb2a70467a291f48
asakuracl <champi.ludo@hotmail.fr>
2020-05-03 14:15:16 -0300

    require bcrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,26 @@
-exports.signup = (req, res, next) => {
+const bcrypt = require('bcrypt')
+const connection = require('../manager')
 
+
+exports.signup = async (req, res, next) => {
+    const password = req.body.password
+    console.log(test)
+    // configurer signup pour creer utilisateur !
 }
+// exports.signup = async (req, res, next) => {
+//     const sql = `
+//         INSERT INTO admin (user, password) 
+//         VALUES ("`+ req.body.firstName + `",
+//                 "`+ req.body.lastName + `",
+//                 "`+ req.body.mail + `",
+//                 "`+ req.body.subject + `",
+//                 "`+ req.body.textArea + `")`
+
+//     connection.query(sql, (error, results, fields) => {
+//         if (error) throw error;
+//         res.send(JSON.stringify(results));
+//     });
+// }
 
 exports.login = (req, res, next) => {
 
