01b427ebe285980f0a97609b5cb2d29aa85ae1b7
asakuracl <champi.ludo@hotmail.fr>
2020-05-05 00:15:30 -0300

    comment signup playing with login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,27 +1,38 @@
 const bcrypt = require('bcrypt')
 const connection = require('../manager')
 
-
 exports.signup = async (req, res, next) => {
-    const password = req.body.password
-    console.log(test)
-    // configurer signup pour creer utilisateur !
+    console.log('dans le signup')
 }
 // exports.signup = async (req, res, next) => {
+//     const password = bcrypt.hash(req.body.password, 10)
+
 //     const sql = `
 //         INSERT INTO admin (user, password) 
-//         VALUES ("`+ req.body.firstName + `",
-//                 "`+ req.body.lastName + `",
-//                 "`+ req.body.mail + `",
-//                 "`+ req.body.subject + `",
-//                 "`+ req.body.textArea + `")`
+//         VALUES ("`+ req.body.user + `",
+//                 "`+ password + `")`
 
 //     connection.query(sql, (error, results, fields) => {
 //         if (error) throw error;
 //         res.send(JSON.stringify(results));
 //     });
+
 // }
 
-exports.login = (req, res, next) => {
 
+exports.login = (req, res, next) => {
+    console.log('dans le login')
+    let user = req.body.user
+    console.log(user)
+    let sql = `SELECT * FROM admin WHERE user = "` + user + `"`
+    console.log(sql)
+    connection.query(sql, (error, results) => {
+        if (results.length > 0 ) {
+            console.log('bon')
+        } else {
+            console.log('faux')
+            throw error
+            res.send(JSON.stringify(results));
+        }
+    });
 }
