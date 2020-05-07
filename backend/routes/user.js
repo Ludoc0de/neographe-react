40fe603801f1018a989474bced6d1bf7bb1ec912
asakuracl <champi.ludo@hotmail.fr>
2020-05-07 17:38:16 -0300

    signup comment

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controllersUser = require('../controllers/user')
 
 
-router.post('/signup', controllersUser.signup);
+//router.post('/signup', controllersUser.signup);
 router.post('/login', controllersUser.login);
 
 module.exports = router
