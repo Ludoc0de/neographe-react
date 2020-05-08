9d98c536913ab994dec7016ea869c8fe4f2a4d3e
asakuracl <champi.ludo@hotmail.fr>
2020-05-08 12:38:04 -0300

    add exports.login and const

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,15 +1,20 @@
 const bcrypt = require('bcrypt')
 const connection = require('../manager')
 
-exports.signup = async (req, res, next) => {
+// exports.signup = async (req, res, next) => {
+//     const user = req.body.user
+//     const password = req.body.password
+//     const passwordHash = await bcrypt.hash(password, 10)
+
+//     const sql = `INSERT INTO admin (user, password) VALUES (?, ?)`
+
+//     connection.query(sql, [user, passwordHash], (error, results, fields) => {
+//         if (error) throw error;
+//         res.status(205).send('signup successful')
+//     });
+// }
+
+exports.login = async (req, res, next) => {
     const user = req.body.user
     const password = req.body.password
-    const passwordHash = await bcrypt.hash(password, 10)
-
-    const sql = `INSERT INTO admin (user, password) VALUES (?, ?)`
-
-    connection.query(sql, [user, passwordHash], (error, results, fields) => {
-        if (error) throw error;
-        res.status(205).send('signup successful')
-    });
-}
+    const sql = `SELECT * FROM admin WHERE user = ?`
