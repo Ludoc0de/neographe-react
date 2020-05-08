36aaa9d4f157ee5cfe86c3520889e4fbde5c6de6
asakuracl <champi.ludo@hotmail.fr>
2020-05-08 12:13:18 -0300

    update sql values, add in passHash 'await'

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,10 +6,7 @@ exports.signup = async (req, res, next) => {
     const password = req.body.password
     const passwordHash = await bcrypt.hash(password, 10)
 
-    const sql = `
-        INSERT INTO admin (user, password) 
-        VALUES ("`+ user + `",
-                "`+ password + `")`
+    const sql = `INSERT INTO admin (user, password) VALUES (?, ?)`
 
     connection.query(sql, (error, results, fields) => {
         if (error) throw error;
