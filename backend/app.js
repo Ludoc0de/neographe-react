78792964064b8df9115c704e2013fd30a178eaf8
asakuracl <champi.ludo@hotmail.fr>
2020-06-28 16:20:16 -0300

    up

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://test.neographe.org/');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
