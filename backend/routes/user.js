b23b15344acbbef1756468514dc1fdf65de0579d
asakuracl <champi.ludo@hotmail.fr>
2020-07-08 21:28:54 -0300

    remove backEnd

--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controllersUser = require('../controllers/user')
-
-
-//router.post('/signup', controllersUser.signup);
-router.post('/login', controllersUser.login);
-
-module.exports = router
